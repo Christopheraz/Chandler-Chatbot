abb0ce0e63fa7da8225670549758e82934d8848c
77 <winky.rain@gmail.com>
2018-12-03 10:06:56 -0500

    delete again

--- README.md
+++
@@ -1,2 +0,0 @@
-# Chandler-Chatbot
-
--- test.py
+++
@@ -1,9 +0,0 @@
-import nltk
-import nltk.data
-
-def splitSentence(paragraph):
-    tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
-    sentences = tokenizer.tokenize(paragraph)
-    return sentences
-if __name__=='__main__':
-    print splitSentence("My name is Tom. I am a boy. I like soccer!")
