e96b93717516db6aa738334edf9cee0e2377e2a8
77 <winky.rain@gmail.com>
2018-12-03 10:10:16 -0500

    delete again and again

--- process_data/create_bash.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-def main():
-    for line in sys.stdin:
-        line = line.strip('\n')
-        print 'python process_data_1.py < ../data/' + line
-
-if __name__ == '__main__':
-    main()
